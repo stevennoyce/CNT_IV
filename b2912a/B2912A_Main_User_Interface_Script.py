efb40983247c4ad71ebbf8b1c218ea67cafcab37
LabMicroscopeComputer <jayldoherty@gmail.com>
2017-12-10 17:47:42 -0500

    Data from C127D 2-3, BiasStress of 1V

--- b2912a/B2912A_Main_User_Interface_Script.py
+++ b2912a/B2912A_Main_User_Interface_Script.py
@@ -10,8 +10,8 @@ import Chip_History as chipHistoryScript
 
 ## ********** Parameters **********
 
-chipID = 'C127E'
-deviceID = '6-7'
+chipID = 'C127D'
+deviceID = '2-3'
 
 # saveFolder = '/Users/stevennoyce/Documents/home/Research/illumina/PSoC/Layout 2_14/Version 2/Host/Testing/'
 # saveFolder = '/Users/jaydoherty/Documents/myWorkspaces/Python/Research/CNT_IV/b2912a/data/'
@@ -63,7 +63,7 @@ additional_parameters = {
 		'time': 600,
 		'complianceCurrent':	100e-6,
 		'gateVoltageSetPoint':	-15.0,
-		'drainVoltageSetPoint':	0.5
+		'drainVoltageSetPoint':	1
 	},
 	'AutoGateSweep':{
 		'numberOfSweeps':6*12,
