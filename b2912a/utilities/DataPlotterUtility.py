1e38f55408a68d095d60cec68ba5856d553d2a51
Jay Doherty <jld60@duke.edu>
2018-01-15 11:20:26 -0500

    Updated plotting

--- b2912a/utilities/DataPlotterUtility.py
+++ b2912a/utilities/DataPlotterUtility.py
@@ -42,10 +42,12 @@ def plotJSON(jsonData, lineColor):
 def plotFullGateSweepHistory(deviceHistory, saveFigure=False, showFigure=True):
 	fig, ax = initFigure(1, 1, 'GateSweep')
 	colors = colorsFromMap(color_maps['GateSweep'], 0.7, 0, len(deviceHistory))
+	indicesToLabel = np.linspace(0, len(deviceHistory)-1, 8).astype(int)
 	for i in range(len(deviceHistory)):
-		plotGateSweep(ax, deviceHistory[i], colors[i])	
-	ax.annotate('Oldest to newest', xy=(0.3, 0.01*len(deviceHistory)), xycoords='axes fraction', fontsize=8, horizontalalignment='left', verticalalignment='bottom', rotation=270)
-	ax.annotate('', xy=(0.29, 0.04), xytext=(0.29,0.0475*len(deviceHistory)), xycoords='axes fraction', arrowprops=dict(arrowstyle='->'))
+		includeLegend = True if(len(deviceHistory) <= 8 or (i in indicesToLabel)) else False
+		plotGateSweep(ax, deviceHistory[i], colors[i], includeLegend)	
+	ax.annotate('Oldest to newest', xy=(0.3, 0.05), xycoords='axes fraction', fontsize=8, horizontalalignment='left', verticalalignment='bottom', rotation=270)
+	ax.annotate('', xy=(0.29, 0.02), xytext=(0.29,0.4), xycoords='axes fraction', arrowprops=dict(arrowstyle='->'))
 	ax.annotate('$V_{DS} = $', xy=(0.05, 0.45), xycoords='axes fraction', horizontalalignment='left', verticalalignment='bottom')
 	adjustFigure(fig, saveFigure, showFigure)
 
@@ -56,7 +58,7 @@ def plotFullBurnOutHistory(deviceHistory, saveFigure=False, showFigure=True):
 	colors = colorsFromMap(color_maps['BurnOut'], 0.6, 1.0, len(deviceHistory))
 	for i in range(len(deviceHistory)):
 		plotBurnOut(ax1, ax2, ax3, deviceHistory[i], colors[i])
-	ax1.annotate('$V_{GS} = 15V$', xy=(0.96, 0.05), xycoords='axes fraction', horizontalalignment='right', verticalalignment='bottom')
+	ax1.annotate('$V_{GS} = $', xy=(0.96, 0.05), xycoords='axes fraction', horizontalalignment='right', verticalalignment='bottom')
 	adjustFigure(fig, saveFigure, showFigure)
 
 def plotFullStaticBiasHistory(deviceHistory, saveFigure=False, showFigure=True):
@@ -76,11 +78,13 @@ def plotOnAndOffCurrentHistory(deviceHistory, saveFigure=False, showFigure=True)
 		onCurrents.append(deviceRun['onCurrent'])
 		offCurrents.append(deviceRun['offCurrent'])
 
-	scatter(ax1, range(len(onCurrents)), onCurrents, 'r', 'On Currents', 6)
+	line = scatter(ax1, range(len(onCurrents)), onCurrents, 'r', 6)
+	setLabel(line, 'On Currents')
 	axisLabels(ax1, 'Data From Subthreshold Sweeps Over Time', 'On Current, $(I_{on})$ [A]')
 	ax1.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
-	scatter(ax2, range(len(offCurrents)), offCurrents, 'b', 'Off Currents', 2)
+	line = scatter(ax2, range(len(offCurrents)), offCurrents, 'b', 2)
+	setLabel(line, 'Off Currents')
 	ax2.set_ylabel('Off Current, $(I_{off})$ [A]')
 	ax2.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 	
@@ -103,8 +107,10 @@ def plotChipOnOffRatios(firstRunChipHistory, recentRunChipHistory):
 
 	lastOnOffRatios, devices, firstOnOffRatios = zip(*(reversed(sorted(zip(lastOnOffRatios, devices, firstOnOffRatios)))))
 
-	scatter(ax, range(len(devices)), firstOnOffRatios, 'b', 'First Run', 6)
-	scatter(ax, range(len(devices)), lastOnOffRatios, 'r', 'Most Recent Run', 4)
+	line = scatter(ax, range(len(devices)), firstOnOffRatios, 'b', 6)
+	setLabel(line, 'First Run')
+	line = scatter(ax, range(len(devices)), lastOnOffRatios, 'r', 4)
+	setLabel(line, 'Most Recent Run')
 	axisLabels(ax, x_label='Device', y_label='On/Off Ratio, $log_{10}(I_{on}/I_{off})$ (Orders of Magnitude)')
 	tickLabels(ax, devices, rotation=90)
 	
@@ -118,29 +124,31 @@ def show():
 
 # ***** Device Plots *****
 
-def plotGateSweep(axis, jsonData, lineColor):
+def plotGateSweep(axis, jsonData, lineColor, includeLabel=True):
 	#scatter(axis, jsonData['gateVoltages'], abs(np.array(jsonData['current1s'])), lineColor, '$I_{on}/I_{off}$'+': {:.1f}'.format(np.log10(jsonData['onOffRatio'])), 3)
-	plotWithErrorBars(axis, jsonData['gateVoltages'], abs(np.array(jsonData['current1s'])), lineColor, '$log_{10}(I_{on}/I_{off})$'+': {:.1f}'.format(np.log10(jsonData['onOffRatio'])))
+	line = plotWithErrorBars(axis, jsonData['gateVoltages'], abs(np.array(jsonData['current1s'])), lineColor)
 	semiLogScale(axis)
 	axisLabels(axis, x_label='Gate Voltage, $V_{GS}$ [V]', y_label='Drain Current, $I_D$ [A]')
-	axis.legend(loc='lower left', fontsize=8) #bbox_to_anchor=(1.25,0.5)
+	if(includeLabel): 
+		setLabel(line, '$log_{10}(I_{on}/I_{off})$'+': {:.1f}'.format(np.log10(jsonData['onOffRatio'])))
+		axis.legend(loc='lower left', fontsize=8) #bbox_to_anchor=(1.25,0.5)
 
 def plotBurnOut(axis1, axis2, axis3, jsonData, lineColor):
-	plot(axis1, jsonData['voltage1s'], (np.array(jsonData['current1s'])*10**6), lineColor, '')
+	plot(axis1, jsonData['voltage1s'], (np.array(jsonData['current1s'])*10**6), lineColor)
 	axisLabels(axis1, x_label='Drain-to-Source Voltage, $V_{DS}$ [V]', y_label='Drain Current, $I_D$ [$\mu$A]')
 
 	currentThreshold = np.percentile(np.array(jsonData['current1s']), 90) * jsonData['thresholdProportion'] * 10**6
 	axis1.plot([0, jsonData['voltage1s'][-1]], [currentThreshold, currentThreshold], color=lineColor, linestyle='--', linewidth=1)
 	axis1.annotate('burn current', xy=(0, currentThreshold), xycoords='data', fontsize=8, horizontalalignment='left', verticalalignment='bottom', color=lineColor)
 	
-	plotOverTime(axis2, jsonData['timestamps'], (np.array(jsonData['current1s'])*10**6), lineColor, '')	
+	plotOverTime(axis2, jsonData['timestamps'], (np.array(jsonData['current1s'])*10**6), lineColor)	
 	axisLabels(axis2, x_label='Time, $t$ [sec]', y_label='Drain Current, $I_D$ [$\mu$A]')
 
-	plotOverTime(axis3, jsonData['timestamps'], jsonData['voltage1s'], lineColor, '')
+	plotOverTime(axis3, jsonData['timestamps'], jsonData['voltage1s'], lineColor)
 	axisLabels(axis3, x_label='Time, $t$ [sec]', y_label='Drain-to-Source Voltage, $V_{DS}$ [V]')
 
 def plotStaticBias(axis, jsonData, lineColor, timeOffset):
-	plotOverTime(axis, jsonData['timestamps'], (np.array(jsonData['current1s'])*10**6), lineColor, '', timeOffset)	
+	plotOverTime(axis, jsonData['timestamps'], (np.array(jsonData['current1s'])*10**6), lineColor, timeOffset)	
 	axisLabels(axis, x_label='Time, $t$ [sec]', y_label='Drain Current, $I_D$ [$\mu$A]')
 
 
@@ -167,23 +175,26 @@ def colorsFromMap(mapName, colorStartPoint, colorEndPoint, numberOfColors):
 
 # ***** Plots ***** 
 
-def plot(axis, x, y, lineColor, lineLabel):
-	axis.plot(x, y, color=lineColor, label=lineLabel)
+def plot(axis, x, y, lineColor):
+	return axis.plot(x, y, color=lineColor)[0]
 
-def scatter(axis, x, y, lineColor, lineLabel, markerSize):
-	axis.plot(x, y, color=lineColor, label=lineLabel, marker='o', markersize=markerSize, markeredgecolor='none', linewidth=0)
+def scatter(axis, x, y, lineColor, markerSize):
+	return axis.plot(x, y, color=lineColor, marker='o', markersize=markerSize, markeredgecolor='none', linewidth=0)[0]
 
-def plotWithErrorBars(axis, x, y, lineColor, lineLabel):
+def plotWithErrorBars(axis, x, y, lineColor):
 	x_unique, avg, std = avgAndStdAtEveryPoint(x, y)
-	axis.errorbar(x_unique, avg, yerr=std, color=lineColor, label=lineLabel, capsize=4, capthick=0.5, elinewidth=0.5,)
+	return axis.errorbar(x_unique, avg, yerr=std, color=lineColor, capsize=4, capthick=0.5, elinewidth=0.5)[0]
 
-def plotOverTime(axis, timestamps, y, lineColor, lineLabel, offset=0):
+def plotOverTime(axis, timestamps, y, lineColor, offset=0):
 	zeroed_timestamps = list( np.array(timestamps) - timestamps[0] + offset )
-	axis.plot(zeroed_timestamps, y, color=lineColor, label=lineLabel, marker='o', markersize = 1, linewidth=1)
+	return axis.plot(zeroed_timestamps, y, color=lineColor, marker='o', markersize = 1, linewidth=1)[0]
 
 
 
 # ***** Labels *****
+
+def setLabel(line, label):
+	line.set_label(label)
 
 def semiLogScale(axis):
 	axis.set_yscale('log')
