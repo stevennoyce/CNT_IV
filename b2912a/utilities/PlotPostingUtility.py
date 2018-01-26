78f000d691ed081883f53c10996be98033071ce0
stevennoyce <steven.noyce@gmail.com>
2018-01-26 08:30:59 -0500

    Starting work on plotPostingUtility

--- b2912a/utilities/PlotPostingUtility.py
+++ b2912a/utilities/PlotPostingUtility.py
@@ -2,26 +2,31 @@ import base64
 import requests
 
 def postPlots(parameters):
-	pass
-
-with open("fig1 3.png", "rb") as imageFile:
-	encodedImage = base64.b64encode(imageFile.read())
-
-postURL = 'https://script.google.com/macros/s/AKfycbzflDpYVTV3NGAEEaC-hfyQTN94JhZbr75dEh_czd7XXN5mDA/exec'
-
-postData = {
-	'chipID': chipID,
-	'deviceID': deviceID,
-	'experimentNumber': experimentNumber,
-	'runType': runType,
-	'encodedImage': encodedImage,
-	'startIndex': 
-	'stopIndex': 
-	'imageName': 
-}
-
-response = requests.post(postURL, data = postData)
-
-print('Posting plot to web service...')
-print(response)
-print(response.text)
+	return
+	if not parameters['postFigures']:
+		return
+	
+	plotFileNames = parameters['figuresSaved']
+	
+	for plotFileName in plotFileNames:
+		with open(plotFileName, "rb") as plotFile:
+			encodedImage = base64.b64encode(plotFile.read())
+		
+		postURL = 'https://script.google.com/macros/s/AKfycbzflDpYVTV3NGAEEaC-hfyQTN94JhZbr75dEh_czd7XXN5mDA/exec'
+		
+		postData = {
+			'chipID': chipID,
+			'deviceID': deviceID,
+			'experimentNumber': experimentNumber,
+			'runType': runType,
+			'encodedImage': encodedImage,
+			'startIndex': 
+			'stopIndex': 
+			'imageName': 
+		}
+		
+		response = requests.post(postURL, data = postData)
+		
+		print('Posting plot to web service...')
+		print(response)
+		print(response.text)
