42b04f9e33372c276810b98de8eff4e03f5818db
stevennoyce <steven.noyce@gmail.com>
2018-01-30 15:12:16 -0500

    Improved and activated plotPostingUtility

--- b2912a/utilities/PlotPostingUtility.py
+++ b2912a/utilities/PlotPostingUtility.py
@@ -1,48 +1,35 @@
-import os
-import glob
 import base64
 import requests
-
-def makeFolder(folderPath):
-	if (not os.path.exists(folderPath)):
-		os.makedirs(folderPath)
+import glob
 
 def postPlots(parameters):
 	print('When entering postPlots(), parameters is:')
 	print(parameters)
 	
-	makeFolder('CurrentPlots')
-	
-	# Exit function while in development so as not to cause errors
-	return
-	
 	if not parameters['postFigures']:
 		return
 	
-	plotFileNames = glob.glob(parameters['plotsFolder'])
+	try:
+		plotFileNames = glob.glob(parameters['plotsFolder'])
+		
+		for plotFileName in plotFileNames:
+			with open(plotFileName, "rb") as plotFile:
+				encodedImage = base64.b64encode(plotFile.read())
+			
+			postURL = 'https://script.google.com/macros/s/AKfycbzflDpYVTV3NGAEEaC-hfyQTN94JhZbr75dEh_czd7XXN5mDA/exec'
+			
+			postData = parameters
+			postData['encodedImage'] = encodedImage
+			postData['imageName'] = plotFileName.split('.')[0]
+			
+			response = requests.post(postURL, data = postData)
+			
+			print('Posting plot to web service...')
+			print(response)
+			print(response.text)
 	
-	for plotFileName in plotFileNames:
-		with open(plotFileName, "rb") as plotFile:
-			encodedImage = base64.b64encode(plotFile.read())
-		
-		postURL = 'https://script.google.com/macros/s/AKfycbzflDpYVTV3NGAEEaC-hfyQTN94JhZbr75dEh_czd7XXN5mDA/exec'
-		
-		postData = {
-			'chipID': parameters['chipID'],
-			'deviceID': parameters['deviceID'],
-			#'experimentNumber': ,
-			'runType': parameters['runType'],
-			'encodedImage': encodedImage,
-			#'startIndex': ,
-			#'stopIndex': ,
-			'imageName': plotFileName.split('.')[0]
-		}
-		
-		response = requests.post(postURL, data = postData)
-		
-		print('Posting plot to web service...')
-		print(response)
-		print(response.text)
+	except:
+		print('Failed to post plots')
 
 
 
