aa2803a1aa56cc11f0e6dc21ebfdab65dd278f49
Jay Doherty <jld60@duke.edu>
2018-02-20 13:45:24 -0500

    Changed command string formatting

--- b2912a/framework/SourceMeasureUnit.py
+++ b2912a/framework/SourceMeasureUnit.py
@@ -235,10 +235,10 @@ class PCB2v14(SourceMeasureUnit):
 			print(self.getResponse())
 
 	def setVds(self, voltage):
-		self.setParameter("set-vds-mv {}!".format(voltage*1000))
+		self.setParameter("set-vds-mv {:d}!".format(voltage*1000))
 
 	def setVgs(self, voltage):
-		self.setParameter("set-vgs-mv {}!".format(voltage*1000))
+		self.setParameter("set-vgs-mv {:d}!".format(voltage*1000))
 
 	def takeMeasurement(self):
 		self.setParameter('measure !')
