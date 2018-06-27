029c72c90ce176286194f3f03b7ecca120839be8
Nathaniel Brooke <neb17@duke.edu>
2018-06-26 20:12:28 -0400

    Updated plotting code to calculate for all runs in a data set

--- b2912a/Data_Plotter.py
+++ b2912a/Data_Plotter.py
@@ -113,23 +113,23 @@ default_parameters = {
 	'NPLC':1
 }
 
-jsonData = dlu.loadJSON('data/' + waferID + '/' + chipID + '/' + deviceID + '/', 'GateSweep.json')[Run_Num]
-parameters = dict(default_parameters)
-id_data = jsonData['Results']['id_data'][0]
-vgs_data = jsonData['Results']['vgs_data'][0]
-fit = dpu.linearFit(vgs_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound], id_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound]);
-print('gm = ' + str(fit['slope']))
-VT = fit['intercept'] / fit['slope']
-print('VT = ' + str(VT))
-fig, axes = dpu.initFigure(parameters, 1, 1, 'TransferCurve', 'Transconductance Model')
-dpu.axisLabels(axes, "$V_{GS}$ (V)", "$I_{D}$ (A)")
-data_line = dpu.scatter(axes, vgs_data, id_data, '#00009c', lineWidth=1)
-dpu.setLabel(data_line, ' $I_{D}$ Data')
-fit_line = dpu.scatter(axes, vgs_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound], fit['fitted_data'], '#ffa500', lineWidth=1)
-dpu.setLabel(fit_line, ' Model')
-axes.legend(loc='best', borderpad=0.15, labelspacing=0.3, handlelength=0.2, handletextpad=0.1)
-saveName = waferID + "_" + chipID + "_" + deviceID + "_" + "Transfer_Curve"
-dpu.adjustFigure(fig, saveName, parameters, saveFigure=True, showFigure=True)
-dpu.show()
+allData = dlu.loadJSON('data/' + waferID + '/' + chipID + '/' + deviceID + '/', 'GateSweep.json')
+for run_num in range(0, len(allData)):
+	jsonData = allData[run_num]
+	parameters = dict(default_parameters)
+	id_data = jsonData['Results']['id_data'][0]
+	vgs_data = jsonData['Results']['vgs_data'][0]
+	fit = dpu.linearFit(vgs_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound], id_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound]);
+	VT = fit['intercept'] / fit['slope']
+	print('gm = ' + str(fit['slope']) + ', VT = ' + str(VT))
+	fig, axes = dpu.initFigure(parameters, 1, 1, 'TransferCurve', 'Transconductance Model, Run ' + str(run_num))
+	dpu.axisLabels(axes, "$V_{GS}$ (V)", "$I_{D}$ (A)")
+	data_line = dpu.scatter(axes, vgs_data, id_data, '#00009c', lineWidth=1)
+	dpu.setLabel(data_line, ' $I_{D}$ Data')
+	fit_line = dpu.scatter(axes, vgs_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound], fit['fitted_data'], '#ffa500', lineWidth=1)
+	dpu.setLabel(fit_line, ' Model')
+	axes.legend(loc='best', borderpad=0.15, labelspacing=0.3, handlelength=0.2, handletextpad=0.1)
+	saveName = waferID + "_" + chipID + "_" + deviceID + "_Run" + str(run_num) + "_Transfer_Curve"
+	dpu.adjustFigure(fig, saveName, parameters, saveFigure=True, showFigure=False)
 
 
