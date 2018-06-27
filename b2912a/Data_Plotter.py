5aa1344f954ab44d7ed8979cc247b9ca25338ab3
Nathaniel Brooke <neb17@duke.edu>
2018-06-26 20:34:35 -0400

    Data Plotter complete, with example output file

--- b2912a/Data_Plotter.py
+++ b2912a/Data_Plotter.py
@@ -6,13 +6,11 @@ waferID = 'C139'
 chipID = 'D'
 deviceID = '1'
 
-# Run Specifier
-Run_Num = 1
-
 # Model Fit Specifier for Data Range
 Transconductance_Lower_Bound = 0
 Transconductance_Upper_Bound = 18
 
+# Parameters matching main code
 default_parameters = {
 	'ParametersFormatVersion': 3,
 	'GateSweep':{
@@ -113,7 +111,11 @@ default_parameters = {
 	'NPLC':1
 }
 
+# Read data from file
 allData = dlu.loadJSON('data/' + waferID + '/' + chipID + '/' + deviceID + '/', 'GateSweep.json')
+
+# Graph and calculate transconductance and threshold voltage
+output = ['gm, VT\n']
 for run_num in range(0, len(allData)):
 	jsonData = allData[run_num]
 	parameters = dict(default_parameters)
@@ -121,8 +123,8 @@ for run_num in range(0, len(allData)):
 	vgs_data = jsonData['Results']['vgs_data'][0]
 	fit = dpu.linearFit(vgs_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound], id_data[Transconductance_Lower_Bound:Transconductance_Upper_Bound]);
 	VT = fit['intercept'] / fit['slope']
-	print('gm = ' + str(fit['slope']) + ', VT = ' + str(VT))
-	fig, axes = dpu.initFigure(parameters, 1, 1, 'TransferCurve', 'Transconductance Model, Run ' + str(run_num))
+	output.append(str(fit['slope']) + ', ' + str(VT) + "\n")
+	fig, axes = dpu.initFigure(parameters, 1, 1, 'TransferCurve', 'Transfer Curve, Run ' + str(run_num))
 	dpu.axisLabels(axes, "$V_{GS}$ (V)", "$I_{D}$ (A)")
 	data_line = dpu.scatter(axes, vgs_data, id_data, '#00009c', lineWidth=1)
 	dpu.setLabel(data_line, ' $I_{D}$ Data')
@@ -131,5 +133,10 @@ for run_num in range(0, len(allData)):
 	axes.legend(loc='best', borderpad=0.15, labelspacing=0.3, handlelength=0.2, handletextpad=0.1)
 	saveName = waferID + "_" + chipID + "_" + deviceID + "_Run" + str(run_num) + "_Transfer_Curve"
 	dpu.adjustFigure(fig, saveName, parameters, saveFigure=True, showFigure=False)
+
+# Output data as CSV file
+file = open("CurrentPlots/" + waferID + "_" + chipID + "_" + deviceID + "_Transconductance_Threshold.csv", "w+")
+file.writelines(output)
+file.close()
 
 
