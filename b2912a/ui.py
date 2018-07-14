7baad1407bfe09fe8d243a696a147d6aaa9941ef
stevennoyce <steven.noyce@gmail.com>
2018-07-14 18:46:53 -0400

    UI properly shows parameters and multiple plots

--- b2912a/ui.py
+++ b2912a/ui.py
@@ -87,5 +87,5 @@ def experiments(wafer, chip, device):
 
 if __name__ == '__main__':
 	url = 'http://127.0.0.1:5000/ui/index.html'
-	webbrowser.open_new(url)
+	# webbrowser.open_new(url)
 	app.run(debug=True)
