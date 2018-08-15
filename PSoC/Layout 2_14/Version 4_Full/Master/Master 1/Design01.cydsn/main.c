4ea83bdc32218c2077b7a44c8698b8f58fe46a44
Jay Doherty <jayldoherty@gmail.com>
2018-08-15 17:07:48 -0400

    Tried to solve "off by 1" errors in the DAC code

--- PSoC/Layout 2_14/Version 4_Full/Master/Master 1/Design01.cydsn/main.c
+++ PSoC/Layout 2_14/Version 4_Full/Master/Master 1/Design01.cydsn/main.c
@@ -505,7 +505,7 @@ void Set_Vds_Raw(uint8 value) {
 	int8 increment = 1;
 	if (istart > value) increment = -1;
 	
-	for (uint8 i = 0; i != value; i += increment) {
+	for (uint8 i = istart; i != (value + increment); i += increment) {
 		if (At_Compliance()) {
 			for (uint8 j = i; j != istart; j -= increment) {
 				VDAC_Vds_SetValue(j);
@@ -530,7 +530,7 @@ void Set_Vgs_Raw(uint8 value) {
 	uint8 istart = VDAC_Vgs_Data;
 	if (value < istart) increment = -1;
 	
-	for (uint8 i = istart; i != value; i += increment) {
+	for (uint8 i = istart; i != (value + increment); i += increment) {
 		if (At_Compliance()) {
 			for (uint8 j = i; j != istart; j -= increment) {
 				VDAC_Vgs_SetValue(j);
@@ -554,7 +554,7 @@ void Set_Ref_Raw(uint8 value) {
 	int8 increment = 1;
 	if (value < VDAC_Ref_Data) increment = -1;
 	
-	for (uint8 i = VDAC_Ref_Data; i != value; i += increment) {
+	for (uint8 i = VDAC_Ref_Data; i != (value + increment); i += increment) {
 		
 		if (At_Compliance()) return;
 		
